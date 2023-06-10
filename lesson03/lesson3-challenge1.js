b516799e81d0ae2a1727a853521829318c99a7fb
rmjanet12 <janetmichaelr@gmail.com>
2023-06-10 13:49:08 -0700

    lesson3-challenge1

---
+++ lesson03/lesson3-challenge1.js
@@ -0,0 +1,12 @@
+let arr=['red','orange','blue','green','yellow'];
+console.log(arr);
+
+arr.push('purple');
+console.log(arr);
+
+arr.splice(2,1);
+console.log(arr);
+
+let arr1;
+arr1=arr.join(',');
+console.log(arr1);
